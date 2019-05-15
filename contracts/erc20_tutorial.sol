cd36134fb6dd1cadd86ac7d1cb93abdb4097cd53
mnrosx <36322372+MNROSX@users.noreply.github.com>
2019-05-15 00:59:33 -0500

    Update erc20_tutorial.sol

--- contracts/erc20_tutorial.sol
+++ contracts/erc20_tutorial.sol
@@ -1,17 +1,16 @@
 pragma solidity ^0.4.24;
 
 // ----------------------------------------------------------------------------
-// '0Fucks' token contract
+// '0x17b815618c3fcb03f608422f60372d5e19f5db6f' token contract
 //
-// Deployed to : 0x5A86f0cafD4ef3ba4f0344C138afcC84bd1ED222
-// Symbol      : 0FUCKS
-// Name        : 0 Fucks Token
+// Deployed to : 0x17b815618c3fcb03f608422f60372d5e19f5db6f
+// Symbol      : ATC
+// Name        : AkitaTokCoin
 // Total supply: 100000000
 // Decimals    : 18
 //
 // Enjoy.
 //
-// (c) by Moritz Neto with BokkyPooBah / Bok Consulting Pty Ltd Au 2017. The MIT Licence.
 // ----------------------------------------------------------------------------
 
 
@@ -99,7 +98,7 @@ contract Owned {
 // ERC20 Token, with the addition of symbol, name and decimals and assisted
 // token transfers
 // ----------------------------------------------------------------------------
-contract FucksToken is ERC20Interface, Owned, SafeMath {
+contract AkitaTokCoin is ERC20Interface, Owned, SafeMath {
     string public symbol;
     string public  name;
     uint8 public decimals;
@@ -113,12 +112,12 @@ contract FucksToken is ERC20Interface, Owned, SafeMath {
     // Constructor
     // ------------------------------------------------------------------------
     constructor() public {
-        symbol = "0FUCKS";
-        name = "0 Fucks Token";
+        symbol = "ATC";
+        name = "AkitaTokCoin";
         decimals = 18;
         _totalSupply = 100000000000000000000000000;
-        balances[0x5A86f0cafD4ef3ba4f0344C138afcC84bd1ED222] = _totalSupply;
-        emit Transfer(address(0), 0x5A86f0cafD4ef3ba4f0344C138afcC84bd1ED222, _totalSupply);
+        balances[0xebe0fd4Bc95487f192F391FB4a354Cef055F17Ab] = _totalSupply;
+        emit Transfer(address(0), 0xebe0fd4Bc95487f192F391FB4a354Cef055F17Ab, _totalSupply);
     }
 
 
